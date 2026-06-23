794138ec3e47db2f695188d1f4013e9c52eace28
KaykeMoraes <moraeskayke01@gmail.com>
2026-06-23 11:51:48 -0300

    fix: fix error in the flake.nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.default = pkgs.mkShell {
+      devShells.${system}.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           go
           gopls
