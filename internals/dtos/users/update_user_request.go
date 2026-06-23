46f1d1a64cefa1017fbe2609bdbf3aee40fbc828
KaykeMoraes <moraeskayke01@gmail.com>
2026-06-23 17:01:48 -0300

    feat: add update user request dto

---
+++ internals/dtos/users/update_user_request.go
@@ -0,0 +1,43 @@
+package users
+
+import (
+	"time"
+
+	"github.com/Projeto-fullstack-UVA/estante-viva-api/internals/entities"
+)
+
+type UpdateUserRequest struct {
+	Name      *string    `json:"name"`
+	Email     *string    `json:"email"`
+	Address   *string    `json:"address"`
+	Document  *string    `json:"document"`
+	Cellphone *string    `json:"cellphone"`
+	Campus    *string    `json:"campus"`
+	BirthDate *time.Time `json:"birthDate"`
+}
+
+func (r UpdateUserRequest) ToModel() entities.User {
+	user := entities.User{}
+	if r.Name != nil {
+		user.Name = *r.Name
+	}
+	if r.Email != nil {
+		user.Email = *r.Email
+	}
+	if r.Address != nil {
+		user.Address = *r.Address
+	}
+	if r.Document != nil {
+		user.Document = *r.Document
+	}
+	if r.Cellphone != nil {
+		user.Cellphone = *r.Cellphone
+	}
+	if r.Campus != nil {
+		user.Campus = *r.Campus
+	}
+	if r.BirthDate != nil {
+		user.BirthDate = *r.BirthDate
+	}
+	return user
+}
